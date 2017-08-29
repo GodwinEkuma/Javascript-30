508e60b8f3401adf2cc81069d278ae21fbafe69b
Godwin Ekuma <godwinekuma@gmail.com>
2017-08-29 10:26:17 +0100

    Add files via upload

---
+++ Javascript 30/01/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', function(e){
+	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if (!audio) return; // stop running function entirely
+
+	audio.currentTime = 0;
+	audio.play(); 
+
+	key.classList.add("playing");
+
+	let keys = document.querySelectorAll(".key")
+
+	keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+	function removeTransition(e){
+		if (e.propertyName !== "transform") return;
+		key.classList.remove("playing");
+	}
+});
